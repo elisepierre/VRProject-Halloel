eba7f1f0a08671c0c1a599482b2e810f9edb7ba7
Elise PIERRE <elise-pierre@outlook.com>
2025-12-18 12:36:38 +0100

    Create Enemy.cs

---
+++ ENEMY/Enemy.cs
@@ -0,0 +1,106 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(Rigidbody))]
+public class Enemy : MonoBehaviour
+{
+    [Header("Stats")]
+    public Transform player;
+    public float contactDamage = 1f;
+
+    [Header("NavMesh")]
+    public float speed = 3f;
+    public float stoppingDistance = 1f;
+
+    [Header("Fall Damage")]
+    public float lethalFallHeight = 5f;
+    private float lastY;
+
+    private NavMeshAgent agent;
+    private bool isStunned = false;
+    private float stunTimer = 0f;
+    private Rigidbody rb;
+
+    private GhostTutorial tutorial;
+
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = speed;
+        agent.stoppingDistance = stoppingDistance;
+        agent.angularSpeed = 120f;
+
+        rb = GetComponent<Rigidbody>();
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+
+        lastY = transform.position.y;
+    }
+
+    private void Start()
+    {
+        tutorial = FindFirstObjectByType<GhostTutorial>();
+
+        if (!agent.isOnNavMesh)
+        {
+            Debug.LogError("Ennemi spawn hors NavMesh !");
+            enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        GhostTutorial tutorial = FindFirstObjectByType<GhostTutorial>();
+
+        if (tutorial != null && tutorial.temporaryMessageActive)
+        {
+            if (agent != null && agent.isOnNavMesh)
+            {
+                agent.isStopped = true;
+                agent.ResetPath(); 
+            }
+            return;
+        }
+        else
+        {
+            if (agent != null && agent.isOnNavMesh)
+                agent.isStopped = false;
+        }
+
+        if (isStunned)
+        {
+            stunTimer -= Time.deltaTime;
+            if (stunTimer <= 0f)
+                isStunned = false;
+            else
+                return;
+        }
+
+        if (player != null && agent.isOnNavMesh)
+            agent.SetDestination(player.position);
+    }
+
+
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        PlayerHealth ph = other.GetComponent<PlayerHealth>();
+        if (ph != null)
+        {
+            Debug.Log("Enemy touche le joueur !");
+            ph.TakeDamage(contactDamage);
+        }
+    }
+
+    public void Stun(float duration)
+    {
+        isStunned = true;
+        stunTimer = duration;
+        if (agent != null && agent.isOnNavMesh)
+            agent.ResetPath();
+
+        Debug.Log(gameObject.name + " stun appliqué!");
+    }
+}
